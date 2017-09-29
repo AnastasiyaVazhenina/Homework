3c5b788180fd5802b3136036cc43ba50ebb30e21
AnastasiyaVazhenina <32188554+AnastasiyaVazhenina@users.noreply.github.com>
2017-09-29 09:02:16 +0300

    Update homework.cpp

--- homework.cpp
+++ homework.cpp
@@ -2,7 +2,7 @@
 int main()
 {
     std::cout << "Enter a three-digit number" << std::endl;
-    int k;
+    int k = 0;
     std::cin >> k;
     int n1 = k % 10;
     int n2 = (k % 100) / 10;
@@ -10,7 +10,7 @@ int main()
     std::cout << n1 << n2 << n3 << std::endl;
     // 7
     std::cout << "Enter the number n (1<=n<=999)" << std::endl;
-    int n;
+    int n = 0;
     std::cin >> n;
     int s = n % 10;
     int x = (n - s) / 10 + s * 100;
